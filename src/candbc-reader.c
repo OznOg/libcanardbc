6e2585c9b6aada14d2515a5bbfbcf977c6275570
Sébastien Gonzalve <sebastien.gonzalve@aliceadsl.fr>
2019-06-02 16:54:27 +0200

    Fix Warnings

--- src/candbc-reader.c
+++ src/candbc-reader.c
@@ -22,23 +22,21 @@ typedef struct yy_buffer_state *YY_BUFFER_STATE;
 #include <candbc-model.h>
 #include <candbc-reader.h>
 
+int yyparse (void *YYPARSE_PARAM);
+void yy_delete_buffer ( YY_BUFFER_STATE b );
+void yyrestart( FILE *input_file );
+YY_BUFFER_STATE yy_create_buffer( FILE *file, int size );
+void yy_switch_to_buffer ( YY_BUFFER_STATE new_buffer );
+
 dbc_t *dbc_read_file(char *filename)
 {
-  void yyrestart( FILE *input_file );
-  YY_BUFFER_STATE yy_create_buffer( FILE *file, int size );
-  void yy_switch_to_buffer ( YY_BUFFER_STATE new_buffer );
-  char *current_yacc_file;
   FILE *yyin;
-  int yyparse (void *YYPARSE_PARAM);
-  void yy_delete_buffer ( YY_BUFFER_STATE b );
 
   int error;
   YY_BUFFER_STATE bufstate;
 
   CREATE(dbc_t, dbc);
   if(dbc != NULL) {
-    current_yacc_file = filename;
-
     if(filename != NULL) {
       yyin = fopen (filename, "r");
       if (yyin == NULL) {
